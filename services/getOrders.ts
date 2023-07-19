e9812359d11f6bf61a1a07adc49a2fd89812301a
daniil2204 <dragindaniilmeizu@gmail.com>
2023-07-19 17:42:14 +0300

    test

--- services/getOrders.ts
+++ services/getOrders.ts
@@ -2,7 +2,8 @@ import { usersOrders } from "@/types";
 
 export const getAllOrders = async () : Promise<Array<usersOrders>> => {
     if (typeof localStorage !== 'undefined') {
-        const token = localStorage.getItem('token');
+        //const token = localStorage.getItem('token');
+        const token = '';
         const responce = await fetch("https://pizzabackend-ames.onrender.com/order", {
             next: {
                 revalidate: 60,
